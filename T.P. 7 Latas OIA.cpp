cc97631fcf0f1153006ddd6d87962c277037abc8
NachoTorress <163569982+NachoTorress@users.noreply.github.com>
2024-04-12 14:01:16 -0300

    Update T.P. 7 Latas OIA.cpp

--- T.P. 7 Latas OIA.cpp
+++ T.P. 7 Latas OIA.cpp
@@ -6,10 +6,8 @@ using namespace std;
 int getLong(string str){
 	return str.size()-1;
 }
-int main(){
-	string str;
-	cin>>str;
-	vector <pair<int, pair<int, int>>> secuencias;
+vector <pair<int, pair<int, int>>> secuencias;
+void getSecuencias(string str){
 	string charAct="";
 	charAct+='G';
 	int inicio=0;
@@ -22,12 +20,30 @@ int main(){
     secuencias.push_back({contador,{inicio, i-1}});
     contador=0;
     inicio=i;
-    }
+}
 	sort (secuencias.rbegin(), secuencias.rend());
+	
+}
+int getPrimeraSecuencia(){
+	return secuencias[0].largo;
+}
+int getSegundaSecuencia(){
+	return secuencias[1].largo;
+}
+int getDistancia(){
+	int dist;
+	if (secuencias[0].hasta<secuencias[1].desde) dist=secuencias[1].desde-secuencias[0].hasta;
+	else dist=secuencias[0].desde-secuencias[1].hasta;
+	return dist;
+}
+int main(){
+	string str;
+	cin>>str;
+	getSecuencias(str);
+
 	cout<<getLong(str)<<endl;
-	cout<<secuencias[0].largo<<endl;
-	cout<<secuencias[1].largo<<endl;
-	if (secuencias[0].hasta<secuencias[1].desde) cout<<secuencias[1].desde-secuencias[0].hasta;
-	else cout<<secuencias[0].desde-secuencias[1].hasta;
+	cout<<getPrimeraSecuencia()<<endl;
+	cout<<getSegundaSecuencia()<<endl;
+	cout<<getDistancia()<<endl;
 }
 
