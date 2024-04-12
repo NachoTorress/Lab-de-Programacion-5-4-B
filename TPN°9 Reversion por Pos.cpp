dee3a547efd4826ac75a9a64276f742fac2099c3
NachoTorress <163569982+NachoTorress@users.noreply.github.com>
2024-04-12 14:46:54 -0300

    Add files via upload

---
+++ TPN°9 Reversion por Pos.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int ini, fin; cin>>ini>>fin;
+	string str="hola", str2; 
+	getline(cin,str);
+	getline(cin,str2);
+	reverse(str2.begin()+ini,str2.begin()+ini+fin+1);
+	cout<<str2;
+}
